d64d6883f3207585c7e8d6a6931d7a1acd79e41f
emapp0451 <erravikant4@gmail.com>
2021-02-15 23:13:50 +0530

    removed step component and added in form

--- src/components/form_component/Form.jsx
+++ src/components/form_component/Form.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import { NotificationContainer, NotificationManager } from 'react-notifications';
-import Step from 'C:/Users/rvknt/installlation_App/installation-App/src/components/general_component/Step'
 class Form extends Component {
     state = {
         taskList: [{ index: Math.random(), statement: "", stepUrl: ""}],
@@ -308,4 +307,27 @@ class Form extends Component {
         )
     }
 }
+const Step = (props) => {
+    return (
+      props.taskList.map((val, idx) => {
+        let statement = `statement-${idx}`, url = `stepUrl-${idx}`
+        return (
+          <tr key={val.index}>
+            <td>
+              <input type="text"  name="statement" data-id={idx} id={statement} className="form-control " />
+            </td>
+            <td>
+              <input type="text"  name="stepUrl" id={url} data-id={idx} className="form-control " />
+            </td>
+            <td>
+              {
+              idx===0?<button onClick={()=>props.add()} type="button" className="btn btn-primary text-center"><i className="fa fa-plus-circle" aria-hidden="true"></i></button>
+              : <button className="btn btn-danger" onClick={(() => props.delete(val))} ><i className="fa fa-minus" aria-hidden="true"></i></button>
+              }
+            </td>
+          </tr >
+        )
+      })
+    )
+  }
 export default Form
